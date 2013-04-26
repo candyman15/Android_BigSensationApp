e5ea4b987624f9399c41cd82b250f74e094d2184
candyman15 <sebastian.park15@gmail.com>
2013-04-26 18:21:48 +0900

    이미지 리스트 및 선택 가능한 화면 관련 부분

--- BigSensation_PreferenceApp/gen/com/dhpreference/R.java
+++ BigSensation_PreferenceApp/gen/com/dhpreference/R.java
@@ -12,8 +12,6 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int jajangmen=0x7f020001;
-        public static final int jjamppong=0x7f020002;
     }
     public static final class id {
         public static final int preferenceintroactivity_bt_gosocial=0x7f060002;
@@ -25,13 +23,9 @@ public final class R {
         public static final int preferencesocialactivity_bt_addfriend=0x7f060006;
         public static final int preferencesocialresult_tv_answermatchnum=0x7f060007;
         public static final int preferencesocialresult_tv_comment=0x7f060008;
-        public static final int preferencetestactivity_bt_next=0x7f06000f;
-        public static final int preferencetestactivity_iv_select1=0x7f06000b;
-        public static final int preferencetestactivity_iv_select2=0x7f06000d;
-        public static final int preferencetestactivity_tv_select1=0x7f06000c;
-        public static final int preferencetestactivity_tv_select2=0x7f06000e;
-        public static final int preferencetestactivity_tv_testnumber=0x7f060009;
-        public static final int preferencetestactivity_tv_testtile=0x7f06000a;
+        public static final int preferencetestactivity_image_table_iv_item=0x7f06000a;
+        public static final int preferencetestactivity_image_table_tv_filename=0x7f06000b;
+        public static final int preferencetestactivity_tl_imagetable=0x7f060009;
     }
     public static final class layout {
         public static final int preferenceintroactivity=0x7f030000;
@@ -39,6 +33,7 @@ public final class R {
         public static final int preferencesocialactivity=0x7f030002;
         public static final int preferencesocialresultactivity=0x7f030003;
         public static final int preferencetestactivity=0x7f030004;
+        public static final int preferencetestactivity_image_table_item=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
--- BigSensation_PreferenceApp/src/com/bigsensation/preference/PreferenceTestActivity.java
+++ BigSensation_PreferenceApp/src/com/bigsensation/preference/PreferenceTestActivity.java
@@ -1,83 +1,162 @@
 package com.bigsensation.preference;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import com.bigsensation.preference.common.TagActivity;
 import com.dhpreference.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class PreferenceTestActivity extends Activity implements OnClickListener{
 
-	private TextView tvTestNumber;
-	private TextView tvTestTitle;
+	private TableLayout tlImageTable;
+	private TableRow tr;
+	private TableRow.LayoutParams params; 
 	
-	private ImageView ivSelect1;
-	private ImageView ivSelect2;
+	private LayoutInflater inflater;
 	
-	private TextView tvSelect1;
-	private TextView tvSelect2;
+	private AssetManager am;
 	
-	private Button btNext;
-	
-	private SharedPreferences prefs = null;
-	
-	int testNum = 0;
+	private String testImageList[];
+	private int testImageNum;	
+	private int trRowNum;
+	private int restImageNum;
+	private int arrCount = 0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.preferencetestactivity);
+		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		tlImageTable = (TableLayout)findViewById(R.id.preferencetestactivity_tl_imagetable);
 		
-		prefs = getSharedPreferences("PrefName", MODE_PRIVATE);
-		testNum = prefs.getInt(TagActivity.TEST_START_NUM, 1);
+		//assets -> testImg 파일 이름 얻어오기 
+		am = getApplicationContext().getResources().getAssets();
+		displayFileName(am,"testImg");
 		
-		tvTestNumber = (TextView)findViewById(R.id.preferencetestactivity_tv_testnumber); // 번호
-		
-		tvTestTitle = (TextView)findViewById(R.id.preferencetestactivity_tv_testtile); // 문제제목
-		
-		tvSelect1 = (TextView)findViewById(R.id.preferencetestactivity_tv_select1);
-		tvSelect2 = (TextView)findViewById(R.id.preferencetestactivity_tv_select2);
-		
-		ivSelect1 = (ImageView)findViewById(R.id.preferencetestactivity_iv_select1); // 선택이미지1
-		ivSelect2 = (ImageView)findViewById(R.id.preferencetestactivity_iv_select2); // 선택이미지2
-		
-		btNext = (Button)findViewById(R.id.preferencetestactivity_bt_next);
-		btNext.setOnClickListener(this);
-		
-		tvTestNumber.setText(String.valueOf(testNum));
-		
-		if(testNum == 1)
+		testImageNum = testImageList.length; //총 이미지 개수
+		restImageNum = testImageNum % 3;
+		if(restImageNum % 3 == 0)
 		{
-			tvTestTitle.setText("둘중에 선택하시오.");
-			ivSelect1.setBackgroundResource(R.drawable.jajangmen);
-			ivSelect2.setBackgroundResource(R.drawable.jjamppong);
-			
-			tvSelect1.setText("짜장면");
-			tvSelect2.setText("짬뽕");
-			
-		}else if(testNum == 2){
-			tvTestTitle.setText("둘중에 좋아하는걸 선택하시오.");
+			trRowNum = testImageNum/3;
 		}
+		else
+		{
+			trRowNum = testImageNum/3 + 1;
+		}
+		arrCount = 0;
+		for(int i=0; i < trRowNum; i++){
+			
+			tr = new TableRow(this);
+			
+			for(int j=0; j < 3 ; j++){
+				LinearLayout table_item = (LinearLayout)inflater.inflate(R.layout.preferencetestactivity_image_table_item, null);
+				final ImageView ivTestImage = (ImageView)table_item.findViewById(R.id.preferencetestactivity_image_table_iv_item);				
+				final TextView tvImageFileName = (TextView)table_item.findViewById(R.id.preferencetestactivity_image_table_tv_filename);				
+				ivTestImage.setImageBitmap(getTestBitmapImage(am,testImageList[arrCount]));
+				tvImageFileName.setText(testImageList[arrCount]);
+				
+				ivTestImage.setOnClickListener(new OnClickListener() {
+					
+					@Override
+					public void onClick(View v) {											
+						Toast.makeText(getApplicationContext(), "파일이름은 : " + tvImageFileName.getText().toString(), Toast.LENGTH_SHORT).show();
+					}
+				});
+				
+				params = new TableRow.LayoutParams(0,400);
+			    params.weight = 3;
+			    
+			    LinearLayout fake_table_item = new LinearLayout(this);
+			    
+			    if(i != trRowNum -1) //마지막 줄이 아닐때 - 무조건 다 돌아라!
+				{
+					tr.addView(table_item, params);
+				}
+				else // 마지막 줄일때는 갯수에 따라 결정
+				{
+					if(restImageNum ==0) // 마지막줄인데 3개일때 - 3의 배수이므로 무조건 다 돌아라!
+					{
+						tr.addView(table_item, params);						
+					}
+					else if(restImageNum ==1)// 마지막줄인데 하나만 있을때 
+					{
+						if(j==0) 
+						{
+							tr.addView(table_item, params);							
+						}
+						else
+						{
+							tr.addView(fake_table_item, params);							
+						}
+					}
+					else if(restImageNum ==2)// 마지막줄인데 두개만 있을때
+					{
+						if(j==0 || j==1) 
+						{
+							tr.addView(table_item, params);							
+						}
+						else 
+						{
+							tr.addView(fake_table_item, params);							
+						}
+					}
+				}
+			    			    			   
+			    if(arrCount < testImageNum-1)
+				{
+					arrCount++;
+				}
+			}
+			tlImageTable.addView(tr);
+		}
+	}
+	
+	private void displayFileName(AssetManager am,String path){
+		
+		 try {
+			  testImageList = am.list(path);
+		        if (testImageList != null)
+		            for (int i=0; i<testImageList.length; ++i)
+		                {
+		                    Log.v("Assets:", path +"/"+ testImageList[i]);		                    
+		                }
+		    } catch (IOException e) {
+		        Log.v("List error:", "can't list" + path);
+		    }
+	}
+	
+	private Bitmap getTestBitmapImage(AssetManager am,String fileName){
+		InputStream is = null;
+		try{			
+			is = am.open("testImg/"+fileName);
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		return BitmapFactory.decodeStream(is);
 	}
 	
 	@Override
 	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.preferencetestactivity_bt_next:
-			SharedPreferences.Editor ed = prefs.edit();
-			ed.putInt(TagActivity.TEST_START_NUM, testNum + 1);			
-			ed.commit();
-			
-			Intent intent = new Intent(this,PreferenceTestActivity.class);
-			startActivity(intent);
-			finish();
-			break;
+		switch (v.getId()) {		
 
 		default:
 			break;
